f6bda862fd7580deab46b6ed9159b35e7219e3cf
Unknown <lostchaos3@gmail.com>
2019-04-14 21:16:18 +0200

    Change UI Color Mod + Latest Progress

---
+++ modloader/thqno_api_x64/Guild3Stuff.h
@@ -0,0 +1,794 @@
+// Generated using ReClass 2016
+
+class G3UIManager;
+class GGraphics;
+class N00000194;
+class N00000197;
+class N0000019A;
+class N0000019D;
+class N000001A0;
+class N000001A3;
+class GEnvironment;
+class N0000525C;
+class N00005263;
+class N0000526E;
+class N00005277;
+class N00005297;
+class N0000529C;
+class N000052A1;
+class N000052A7;
+class N000052B7;
+class N000052BA;
+class N000052C0;
+class N000052C5;
+class N000052D1;
+class N000052D6;
+class N0000530B;
+class N00005310;
+class N00005316;
+class N0000531B;
+class N00005320;
+class N00005326;
+class SFontStructures;
+class N000053CD;
+class N000053D3;
+class GFamily;
+class N000054BF;
+class N000054C2;
+class N000054DE;
+class N000054ED;
+class N0000557E;
+class N00005581;
+class N00005604;
+class N00005608;
+class N00005613;
+class N00005618;
+class N0000561B;
+class N00005621;
+class N00005624;
+class N00005627;
+class N0000562A;
+class N00005639;
+class N0000563C;
+class N0000563F;
+class N00005642;
+class N0000564D;
+class N00005650;
+class N0000565C;
+class GUIRenderer;
+class N00002863;
+class GTVector2INTFloatMath;
+class N0000289C;
+class _UIControl;
+class GClass;
+class GHString;
+class GObject;
+class N000028C9;
+class GObject;
+class N00002B1F;
+class N00002B24;
+class _UIVisual;
+class N00002B2F;
+class N00002B3A;
+class GBasicStringwchar_t;
+class N00002B45;
+class N00002B53;
+class N00002B57;
+class N0000289D;
+class N000028A0;
+class N000028A3;
+class N000028A6;
+class N000028A9;
+class N000028AC;
+class N000028AF;
+
+class GGraphics
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+
+
+class N00000194
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N00000197
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N0000019A
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N0000019D
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N000001A0
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N000001A3
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class GEnvironment
+{
+public:
+	virtual void Function0(); //
+	virtual void Function1(); //
+	virtual void Function2(); //
+	virtual void Function3(); //
+	virtual void Function4(); //
+	virtual void Function5(); //
+	virtual void Function6(); //
+	virtual void Function7(); //
+	virtual void Function8(); //
+	virtual void Function9(); //
+
+	__int32 ReferenceCount; //0x0008 
+	byte pad[4]; //0x000C 
+	N000053CD* Parent; //0x0010 
+	GEnvironment* mRoot; //0x0018 
+	N000053D3* mSymbols; //0x0020 
+	__int32 mCallDepth; //0x0028 
+	unsigned char mContextFlags; //0x002C 
+	unsigned char mHasRun; //0x002D 
+	unsigned char mTempEnv; //0x002E 
+
+}; //Size=0x0407
+class GHString
+{
+public:
+	DWORD64 mHash; //0x0000 
+
+}; //Size=0x0008
+
+class N0000525C
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N00005263
+{
+public:
+	float N00005264; //0x0000 
+	float N00005266; //0x0004 
+
+}; //Size=0x0008
+
+class N0000526E
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N00005277
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N00005297
+{
+public:
+	char pad_0x0000[0x48]; //0x0000
+
+}; //Size=0x0048
+
+class N0000529C
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N000052A1
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N000052A7
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N000052B7
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N000052BA
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N000052C0
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N000052C5
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N000052D1
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N000052D6
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N0000530B
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N00005310
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N00005316
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N0000531B
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N00005320
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N00005326
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class SFontStructures
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N000053CD
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N000053D3
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class GFamily
+{
+public:
+	virtual void Function0(); //
+	virtual void Function1(); //
+	virtual void Function2(); //
+	virtual void Function3(); //
+	virtual void Function4(); //
+	virtual void Function5(); //
+	virtual void Function6(); //
+	virtual void Function7(); //
+	virtual void Function8(); //
+	virtual void Function9(); //
+
+
+	char pad; //0x0000 
+	__int32 ReferenceCount; //0x0008 
+
+
+}; //Size=0x0414
+
+class N000054BF
+{
+public:
+	N000054C2* N000054C0; //0x0000 
+
+}; //Size=0x0008
+
+class N000054C2
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N000054DE
+{
+public:
+	N00005639* N000054DF; //0x0000 
+	char pad_0x0008[0x38]; //0x0008
+	N0000564D* N0000564B; //0x0040 
+
+}; //Size=0x0048
+
+class N000054ED
+{
+public:
+	char pad_0x0000[0x408]; //0x0000
+
+}; //Size=0x0408
+
+class N0000557E
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N00005581
+{
+public:
+	char pad_0x0000[0x408]; //0x0000
+
+}; //Size=0x0408
+
+class N00005604
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N00005608
+{
+public:
+	char pad_0x0000[0x48]; //0x0000
+
+}; //Size=0x0048
+
+class N00005613
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N00005618
+{
+public:
+	N0000561B* N00005619; //0x0000 
+
+}; //Size=0x0008
+
+class N0000561B
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N00005621
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N00005624
+{
+public:
+	N00005627* N00005625; //0x0000 
+
+}; //Size=0x0008
+
+class N00005627
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N0000562A
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N00005639
+{
+public:
+	N0000563C* N0000563A; //0x0000 
+
+}; //Size=0x0008
+
+class N0000563C
+{
+public:
+	N0000563F* N0000563D; //0x0000 
+
+}; //Size=0x0008
+
+class N0000563F
+{
+public:
+	N00005642* N00005640; //0x0000 
+
+}; //Size=0x0008
+
+class N00005642
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N0000564D
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N00005650
+{
+public:
+	char pad_0x0000[0x48]; //0x0000
+
+}; //Size=0x0048
+
+class N0000565C
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+class GBasicStringwchar_t
+{
+public:
+	wchar_t* mString; //0x0000 
+
+}; //Size=0x0008
+
+class GUIRenderer
+{
+public:
+	unsigned char mCurrentlyRendering; //0x0000 
+	unsigned char pad[0x07]; //0x0001 
+	GGraphics* mGraphics; //0x0008 
+	float mPixelOffset; //0x0010 
+	float padding; //0x0014 
+	char pad_0x0018[0x88]; //0x0018
+
+}; //Size=0x00A0
+
+class N00002863
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class GTVector2INTFloatMath
+{
+public:
+	__int32 x; //0x0000 
+	__int32 y; //0x0004 
+	__int32 u; //0x0008 
+	__int32 v; //0x000C 
+
+
+}; //Size=0x0010
+
+class N0000289C
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class _UIControl
+{
+public:
+	GClass* _Class; //0x0000 
+	char pad_s0x0044[0x32]; ; //0x0008 
+	__int32 mID; //0x0040 
+	char pad_0x0044[0x4]; //0x0044
+	GHString N00002AC2; //0x0048 
+	float mOpacity; //0x0050 
+	__int32 mContext; //0x0054 
+	GObject* N00002AC5; //0x0058 
+	float Colorsmtha; //0x0060 
+	float Colorsmthb; //0x0064 
+	_UIVisual* mVisual; //0x0068 
+	GHString mClipMask; //0x0070 
+	unsigned char mIgnoreHideUI; //0x0078 
+	unsigned char mHandleScriptMouseEvents; //0x0079 
+	char pad_0x0044x[0x6]; //0x007A 
+	GHString mAcceptDrop; //0x0080 
+	GBasicStringwchar_t mTooltip; //0x0088 
+	unsigned char mShowLongTooltip; //0x0090 
+	unsigned char mContainsScroll; //0x0091 
+	unsigned char mOverrideLongTooltip; //0x0092 
+	char pad_0x0044y[0x5];  //0x0093 
+	GHString mOverrideLongTooltipStr; //0x0098 
+
+}; //Size=0x00A0
+
+class GClass
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+
+
+class GObject
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N00002B1F
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N00002B24
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class _UIVisual
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N00002B2F
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N00002B3A
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+
+class N00002B45
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N00002B53
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N00002B57
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N0000289D
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N000028A0
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N000028A3
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N000028A6
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N000028A9
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N000028AC
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+class N000028AF
+{
+public:
+	char pad_0x0000[0x8]; //0x0000
+
+}; //Size=0x0008
+
+
+class G3UIManager
+{
+public:
+	virtual void Function0(); //
+	virtual void Function1(); //
+	virtual void Function2(); //
+	virtual void Function3(); //
+	virtual void Function4(); //
+	virtual void Function5(); //
+	virtual void Function6(); //
+	virtual void Function7(); //
+	virtual void Function8(); //
+	virtual void Function9(); //
+
+	SFontStructures* mFontStructures; //0x0008 
+	bool mPreventClicks; //0x0010 
+	bool mPreventCamera; //0x0011 
+	bool mHideUI; //0x0012 
+	bool Padding; //0x0013 
+	__int32 mHideUIFrameCount; //0x0014 
+	GUIRenderer mUIRenderer; //0x0018 
+	GTVector2INTFloatMath mDimensions; //0x00B8 
+	GTVector2INTFloatMath mMousePos; //0x00C8 
+	_UIControl* mLastMouseMove; //0x00D8 
+	_UIControl* mCurrentMouseMove; //0x00E0 
+	_UIControl* mLastMouseMoveIgnoreEnable; //0x00E8 
+	_UIControl* mLastMouseClicked; //0x00F0 
+	_UIControl* mLastMouseReleased; //0x00F8 
+	_UIControl* mFocusedControl; //0x0100 
+	_UIControl* mKBControl; //0x0108 
+
+
+	/*mControlsData; //0x0180 
+	LastMouseButtonClicked5; //0x2198 
+	__int32 padding; //0x21AC 
+	mRemoveList; //0x21B0 
+	mAddList; //0x21C8 
+	mFonts; //0x21E0 
+	mGlobalControls; //0x21F0 
+	mUpdateList; //0x2200 
+	mUpdateRemovalList; //0x2218 
+	float mLastDeltaT; //0x2230 
+	float padd; //0x2234 
+	mPackageLoaderList; //0x2238 
+	GEnvironment* mScriptEnvironment; //0x2250 
+	N0000525C* mTimer; //0x2258 
+	N00005263 mGlobalScale[1]; //0x2260 
+	isIterating / resized / editinglayout; //0x2268 
+	pAdding; //0x226B 
+	N0000526E* mInputHandler; //0x2270 
+	transferobjectvisible / disableworldobjectselection / mtutorialoverride; //0x2278 
+	N00005272; //0x227B 
+	N00005277* mTutorialWindow; //0x2280 
+	N0000527A; //0x2289 
+	mRapidXmlData; //0x2290 
+	mActivePanels; //0x22A8 
+	mPanelToLoad; //0x22C0 
+	mAnchorsType; //0x22D0 
+	mPanels; //0x22E0 
+	mWarningPanels; //0x22F0 
+	mWarningPanelsToRemove; //0x2300 
+	mBluePrints; //0x2310 
+	mBluePrintsDocuments; //0x2320 
+	mMaterials; //0x2338 
+	mPanelFactories; //0x2348 
+	N00005297* mCurrentSelection; //0x2368 
+	N0000529C* mPreviousSelection; //0x2370 
+	N000052A1* mDraggedControl; //0x2378 
+	N000052A7* mLastMouseOverControl; //0x2380 
+	N000052B7* mLastMouseOverPanel; //0x2388 
+	N000052BA* mDefaultToolTipPanel; //0x2390 
+	N000052C0* mCurrentToolTipPanel; //0x2398 
+	N000052C5* mCurrentHintPanel; //0x23A0 
+	float mToolTipTimer; //0x23A8 
+	float mToolTipTimer2; //0x23AC 
+	N000052D1* mToolTipParentControl; //0x23B0 
+	N000052D6* mTooltipLevel3; //0x23B8 
+	mDragging etc; //0x23C0 
+	N000052ED; //0x23C5 
+	mDragStartPosition; //0x23C8 
+	mNextSelection; //0x23D0 
+	mWasSearchingForSelection; //0x2408 
+	N000052FF; //0x2409 
+	mButtonDelegates; //0x2410 
+	mResources; //0x2420 
+	N0000530B* mDynastyPanel; //0x2430 
+	N00005310* mActionPanel; //0x2438 
+	N00005316* mHiringWindow; //0x2440 
+	N0000531B* mDragAndDropItemsWindow; //0x2448 
+	N00005320* mDragAndDropSimpleItemsWindow; //0x2450 
+	N00005326* mDragAndDropEmployeeWindow; //0x2458 
+	char pad_0x2460[0xDEA0]; //0x2460*/
+
+}; //Size=0x10300
--- modloader/thqno_api_x64/thqno_api_x64.cpp
+++ modloader/thqno_api_x64/thqno_api_x64.cpp
@@ -4,8 +4,12 @@
 #include <fcntl.h>
 #include "Guild3Stuff.h"
 #include <iostream>
-#include <iostream>
+#include <memory>
+
+#include "headers/Virtuals/VFuncSwapHook.hpp"
 HINSTANCE mHinst = 0, mHinstDLL = 0;
+PLH::VFuncMap origVFuncs2;
+
 extern "C" UINT_PTR mProcs[53] = {0};
 void Console() {
 	AllocConsole();
@@ -31,28 +35,60 @@ extern "C" void THQNO_AutoInit_wrapper() {
 	
 		(UINT_PTR)GetProcAddress(mHinstDLL, "THQNO_AutoInit")();
 
-		printf("After : %p", GetProcAddress(mHinstDLL, "THQNO_AutoInit"));
+		printf("After : %p\n", GetProcAddress(mHinstDLL, "THQNO_AutoInit"));
 	
 
 }
+class G3UIHook {
+public:
+	virtual int NoParamVirt() {
+		return 4;
+	}
+
+};
+G3UIManager* g3UIManager;
+typedef uintptr_t(__thiscall* tVirtNoParams)(uintptr_t pThis);
+uintptr_t __fastcall uiHOOK(uintptr_t pThis, void* edxDEAD) {
+	printf("G3UIManager= %p\n",pThis);
+	 g3UIManager = (G3UIManager*)pThis;
+	 g3UIManager->mHideUI = true;
+	return ((tVirtNoParams)origVFuncs2.at(1))(pThis);
+}
+float lerp(float a, float b, float f)
+{
+	return a + f * (b - a);
+}
 DWORD WINAPI myThread(LPVOID lpParameter)
 {
+	PLH::VFuncMap redirect = { {(uint16_t)1, (uint64_t)& uiHOOK} };
+	PLH::VFuncSwapHook hook((uint64_t)0x141563A70, redirect, &origVFuncs2);
+	printf("Hook Result: %d\n",hook.hookSimple());
 	//Sleep(5000);
-
-	THQNO_AutoInit_wrapper();
-	return 0;
+	//THQNO_AutoInit_wrapper();
+	while (true)
+	{
+		Sleep(1000);
+		if (g3UIManager) {
+			//g3UIManager->mHideUI = !g3UIManager->mHideUI;
+			if (g3UIManager->mLastMouseMove) {
+				g3UIManager->mLastMouseMove->Colorsmtha  = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+				g3UIManager->mLastMouseMove->Colorsmthb = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+			}
+		}
+	}
+	//return 0;
 }
 
 LPCSTR mImportNames[] = {"LZ4_compress", "LZ4_compressBound", "LZ4_compress_continue", "LZ4_compress_default", "LZ4_compress_destSize", "LZ4_compress_fast", "LZ4_compress_fast_continue", "LZ4_compress_fast_extState", "LZ4_compress_limitedOutput", "LZ4_compress_limitedOutput_continue", "LZ4_compress_limitedOutput_withState", "LZ4_compress_withState", "LZ4_create", "LZ4_createStream", "LZ4_createStreamDecode", "LZ4_decompress_fast", "LZ4_decompress_fast_continue", "LZ4_decompress_fast_usingDict", "LZ4_decompress_fast_withPrefix64k", "LZ4_decompress_safe", "LZ4_decompress_safe_continue", "LZ4_decompress_safe_partial", "LZ4_decompress_safe_usingDict", "LZ4_decompress_safe_withPrefix64k", "LZ4_freeStream", "LZ4_freeStreamDecode", "LZ4_loadDict", "LZ4_resetStream", "LZ4_resetStreamState", "LZ4_saveDict", "LZ4_setStreamDecode", "LZ4_sizeofState", "LZ4_sizeofStreamState", "LZ4_slideInputBuffer", "LZ4_uncompress", "LZ4_uncompress_unknownOutputSize", "LZ4_versionNumber", "LZ4_versionString", "THQNO_Activity", "THQNO_Apps", "THQNO_AutoInit", "THQNO_Chat", "THQNO_Init", "THQNO_Matchmaking", "THQNO_Networking", "THQNO_Process", "THQNO_RegisterCallback", "THQNO_RestartAppIfNecessary", "THQNO_Shutdown", "THQNO_Stats", "THQNO_UnregisterCallback", "THQNO_User", "THQNO_Utils"};
 BOOL WINAPI DllMain( HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved ) {
 	mHinst = hinstDLL;
 	if ( fdwReason == DLL_PROCESS_ATTACH ) {
-		HINSTANCE hGetProcIDDLL = LoadLibrary("guild3mod.dll");
+		/*HINSTANCE hGetProcIDDLL = LoadLibrary("guild3mod.dll");
 
 		if (!hGetProcIDDLL) {
 			std::cout << "could not load the dynamic library" << std::endl;
 			return EXIT_FAILURE;
-		}
+		}*/
 		mHinstDLL = LoadLibrary( "ori_thqno_api_x64.dll" );
 		if ( !mHinstDLL )
 			return ( FALSE );
@@ -61,6 +97,7 @@ BOOL WINAPI DllMain( HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved ) {
 		}
 		//Sleep(5000);
 		Console();
+		//Hook();
 		HANDLE myHandle = CreateThread(0, 0, myThread, NULL , 0, NULL);
 		
 	} else if ( fdwReason == DLL_PROCESS_DETACH ) {
